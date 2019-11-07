950babe93ec71a50d05fde5f78779be5a5886ff6
Viettasc <viettasc@gmail.com>
2019-11-07 09:51:44 +0700

    Initial Commit

---
+++ Trimmed_Path_Loader_v1Tests/Trimmed_Path_Loader_v1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Trimmed_Path_Loader_v1Tests.swift
+//  Trimmed_Path_Loader_v1Tests
+//
+//  Created by Viettasc on 11/7/19.
+//  Copyright © 2019 Viettasc. All rights reserved.
+//
+
+import XCTest
+@testable import Trimmed_Path_Loader_v1
+
+class Trimmed_Path_Loader_v1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
